5f55249d1db966111365ffce1500371a9a0e1ff3
Tanish <tanishmalekar32@gmail.com>
2021-10-11 13:27:27 +0530

    frontend added

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const CryptoJS = require("crypto-js");
+const ejs = require("ejs");
+
 
 const app = express()
 const port = 8000
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
 
 
 mongoose.connect("mongodb+srv://tanish:password%23123@cluster0.ouv6u.mongodb.net/ISAALABDA4?retryWrites=true&w=majority", {useNewUrlParser: true});
@@ -16,6 +21,23 @@ const User = mongoose.model("user", new mongoose.Schema({
     regno: String,
     password: String
 }));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get('/addPage', (req, res) => {
+    res.sendFile(__dirname + "/add.html");
+})
+app.get('/deletePage', (req, res) => {
+    res.sendFile(__dirname + "/delete.html");
+})
+app.get('/modifyPage', (req, res) => {
+    res.sendFile(__dirname + "/modify.html");
+})
+
+
+
 
 app.post('/add', async (req, res) => {
     var regno = req.body.regno;
@@ -31,7 +53,7 @@ app.post('/add', async (req, res) => {
     });
 
     await user.save();
-    res.send("User added successfully!");
+    res.render("success", {msg: "User added successfully!"});
 })
 
 app.post('/delete', async (req, res) => {
@@ -47,10 +69,10 @@ app.post('/delete', async (req, res) => {
         }
 
         if(user===null || password!=user.password){
-            res.send("Regno or password incorrect!");
+            res.render("incorrect", {path: "/deletePage"});
         }else{
             await User.deleteOne({regno: regno});
-            res.send("User deleted successfully!");
+            res.render("success", {msg: "User deleted successfully!"});
         }
     });
 
@@ -59,7 +81,12 @@ app.post('/delete', async (req, res) => {
 app.get('/view', async (req, res) => {
 
     User.find({}, async (err, users) => {
-        res.send(users);
+        users.forEach((user,i)=>{
+            //decrypting password
+            var bytes  = CryptoJS.AES.decrypt(user.password, 'secret key 123');
+            users[i].decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+        })
+        res.render("view", {users: users});
     });
 
 })
@@ -83,10 +110,10 @@ app.post('/modify', async (req, res) => {
         }
 
         if(user===null || currentPassword!=user.password){
-            res.send("Regno or password incorrect!");
+            res.render("incorrect", {path: "/modifyPage"});
         }else{
             await User.updateOne({ regno: currentRegno }, { regno: newRegno, password: newPassword });
-            res.send("Data successfully updated!")
+            res.render("success", {msg: "Data modified successfully!"});
         }
     });
 
